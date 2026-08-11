cc4e24faecdce2b64de505c8ec8806e63dac6b59
Ziah Cunningham <ziahcunningham@gmail.com>
2026-08-11 15:29:06 -0700

    responsive design started

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import "./vendor/fonts.css";
-import "./vendor/normalize.css";
 import App from "./Components/App/App";
 
 createRoot(document.getElementById("root")).render(
